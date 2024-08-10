3c7f32cd7b911302ec85f379d11f63d03820b36a
Alexander SABOURET <asaboure@student.42.fr>
2024-08-10 21:39:39 +0200

    feat: Load thetas from environment variables in estimate.py

--- estimate.py
+++ estimate.py
@@ -1,5 +1,12 @@
-theta0 = 0
-theta1 = 0
+from dotenv import load_dotenv
+import os
+
+# load environment variables
+load_dotenv()
+
+# grab thetas from environment variables
+theta0 = float(os.getenv("THETA0"))
+theta1 = float(os.getenv("THETA1"))
 
 def estimate_price(mileage):
     return theta0 + theta1 * mileage
